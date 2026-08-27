0b4765fb6e7e937bf1f1372068ad089661f48173
fevanigelin07-ui <f.evanigelin07@gmail.com>
2026-08-27 16:51:03 +0530

    Add User model with basic fields

---
+++ models.py
@@ -0,0 +1,9 @@
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from database import Base
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    email = Column(String, unique=True)
+    password = Column(String)
+    role = Column(String, default="farmer")
